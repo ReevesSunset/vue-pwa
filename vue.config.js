b32687ae743a7f40026ac996ce74b6b76b76fbc7
reeves <zhaodi@wecash.net>
2019-08-02 15:08:43 +0800

    deploy

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+// vue.config.js
+module.exports = {
+    pwa: {
+        name: 'My App',
+        themeColor: '#4DBA87',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+    
+        // 配置 workbox 插件
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+          // InjectManifest 模式下 swSrc 是必填的。
+          swSrc: 'dev/sw.js',
+          // ...其它 Workbox 选项...
+        }
+    }
+}
